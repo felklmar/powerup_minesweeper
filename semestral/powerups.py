efdd4d34d68c63ff84c066b53758fc08b44cd84f
Martin Felkl <felklmar@fit.cvut.cz>
2023-01-03 21:46:06 +0100

    Create open bubble powerup

--- semestral/powerups.py
+++ semestral/powerups.py
@@ -1,4 +1,6 @@
+import random as rd
 import pygame as pg
+import numpy as np
 from button import Button
 from minefield import Minefield, OUT_OF_BOUNDS
 
@@ -9,7 +11,7 @@ class Powerup( Button ):
     def apply_powerup( self, minefield : Minefield ) -> bool:
         return False
 
-class Safe_open( Powerup ):
+class Safe_Open( Powerup ):
     def __init__( self, coords : tuple, dim : tuple, color : tuple ):
         super().__init__( coords, dim, color )
 
@@ -26,4 +28,48 @@ class Safe_open( Powerup ):
             return True
 
         return False
-        
+
+class Open_Bubble( Powerup ):
+    def __init__( self, coords : tuple, dim : tuple, color : tuple ):
+        super().__init__( coords, dim, color )
+
+    def apply_powerup( self, minefield: Minefield ) -> bool:
+        c_click = minefield.mouse_pos_to_coords( pg.mouse.get_pos() )
+        if c_click != OUT_OF_BOUNDS:
+            #self.find_bubble( minefield )
+            #minefield.display_field()
+            #return True
+
+            check = 0
+            while True:
+                if check % minefield.m_field.size == 0:
+                    if self.no_bubbles( minefield ):
+                        break
+
+                c_tile = ( rd.randint( 0, minefield.height() - 1 ), rd.randint( 0, minefield.width() - 1 ) )
+                tile = minefield.m_field[c_tile]
+                if not tile.is_open() and not tile.is_mine() and tile.mines_around() == 0:
+                    minefield.open( c_tile )
+                    minefield.display_field()
+                    break
+
+            return True
+
+        return False
+
+    def no_bubbles( self, minefield : Minefield ) -> bool:
+        for row in minefield.m_field:
+            for tile in row:
+                if not tile.is_open() and not tile.is_mine() and tile.mines_around() == 0: 
+                    return False
+
+        return True
+
+    #def find_bubble( self, minefield : Minefield ):
+    #    rot_degree = rd.randint( 1, 4 )
+    #    field = np.rot90( minefield.m_field, rot_degree )
+    #    for row in field:
+    #        for tile in row:
+    #            if not tile.is_open() and not tile.is_mine() and tile.mines_around() == 0:
+    #                minefield.open( tile.arr_coords() )
+    #                return
