6a34278ca306c9e9f1473723bd19f57e54bae4f2
Martin Felkl <felklmar@fit.cvut.cz>
2023-01-07 20:41:09 +0100

    PEP8tize powerups.py

--- semestral/powerups.py
+++ semestral/powerups.py
@@ -1,3 +1,7 @@
+"""
+__Module handling powerups__
+Contains Powerup class and its children
+"""
 import random as rd
 import pygame as pg
 import numpy as np
@@ -5,30 +9,56 @@ from button import Button
 from minefield import Minefield
 from utilities import FONT, OFFSET, OUT_OF_BOUNDS, COLORS
 
-def random_bool( prob : int ) -> bool:
-    return rd.random() < prob
-
 class Powerup( Button ):
-    def __init__( self, coords : tuple, color : tuple, description : list, value = 0 ):
+    """Class inherited from class Button representing powerup"""
+    def __init__( self, coords : tuple, color : tuple, description : tuple, value = 0 ):
+        """
+        __Constructor for class instance__\n
+        Creates and initializes powerup class instance\n
+        Args:
+            coords (tuple): coordinates for powerup button
+            color (tuple): text color
+            description (tuple): name, button text and description
+            value (int, optional): cost of powerup. Defaults to 0.
+        """
         text = ( pg.font.Font( FONT, 17 ), description[0][0], description[0][1]  )
         super().__init__( coords, color, text )
 
-        self.m_description = description
-        self.m_last_coords = OUT_OF_BOUNDS
-        self.m_value = value
+        self.m_description = description    # powerup description
+        self.m_value = value                # value/cost of powerup
+
+        # deactivation of powerup
         self.deactivate()
 
-    def __str__( self ) -> str:
-        return self.m_description
+    def apply_powerup( self, window : pg.Surface, minefield: Minefield ) -> str:
+        """
+        Pure virtual method, that applies powerup\n
+        Args:
+            window (pg.Surface): pygame window/surface on which powerup button should be print
+            minefield (Minefield): minefield on which are powerups applied
+        Returns:
+            str: 'applied', 'used', 'not_used'
+        """
 
     def deactivate( self ):
-        self.m_color = COLORS['dis_text']
+        """Deactivates powerup"""
+        self.m_color = COLORS['t_disabled']
 
-    def activate( self, tokens ):
+    def activate( self, tokens : int ):
+        """
+        Activates powerup if powerup tokens are anough to pay for it\n
+        Args:
+            tokens (int): powerup tokens
+        """
         if tokens >= self.m_value:
-            self.m_color = self.m_default_color
+            self.m_color = COLORS['t_basic']
 
     def display_description( self, window : pg.Surface ):
+        """
+        Displays powerup description on the pygame window\n
+        Args:
+            window (pg.Surface): pygame window/surface on which tile should display
+        """
         center_x = window.get_width()/2
         font = pg.font.Font( FONT, 15 )
         text = font.render( self.m_description[1], True, self.m_color )
@@ -37,11 +67,33 @@ class Powerup( Button ):
         window.blit( text, t_rect )
 
 class SafeOpen( Powerup ):
+    """
+    __Class inherited from class Powerup__\n
+    Safely reveals tile ( either opens it or flags it )
+    """
     def __init__( self, coords : tuple, color : tuple, value ):
-        description = ( ( 'SafeOpen', f'({ value }) Reveal' ), 'Reveals selected tile' )
+        """
+        __Constructor for class instance__\n
+        Creates and initializes powerup class instance\n
+        Args:
+            coords (tuple): coordinates for powerup button
+            color (tuple): text color
+            value (int, optional): cost of powerup. Defaults to 0.
+        """
+        description = ( ( 'SafeOpen', f'({ value }) Reveal' ),
+                        'Reveals selected tile' )
         super().__init__( coords, color, description, value )
 
-    def apply_powerup( self, window : pg.Surface, minefield: Minefield ) -> bool:
+    def apply_powerup( self, window : pg.Surface, minefield: Minefield ) -> str:
+        """
+        __Overrided parent method, that applies powerup__\n
+        Safely reveals tile ( either opens it or flags it )
+        Args:
+            window (pg.Surface): pygame window/surface on which powerup button should be print
+            minefield (Minefield): minefield on which are powerups applied
+        Returns:
+            str: 'applied', 'used', 'not_used'
+        """
         c_click = minefield.mouse_pos_to_coords( pg.mouse.get_pos() )
         if c_click != OUT_OF_BOUNDS:
             tile = minefield.m_field[c_click]
@@ -60,11 +112,33 @@ class SafeOpen( Powerup ):
         return 'not_used'
 
 class OpenBubble( Powerup ):
+    """
+    __Class inherited from class Powerup__\n
+    Opens random bubble ( tile with zero mines around ) if there is one
+    """
     def __init__( self, coords : tuple, color : tuple, value ):
-        description = ( ( 'OpenBubble', f'({ value }) Bubble' ), 'Reveals random bubble, if there is one' )
+        """
+        __Constructor for class instance__\n
+        Creates and initializes powerup class instance\n
+        Args:
+            coords (tuple): coordinates for powerup button
+            color (tuple): text color
+            value (int, optional): cost of powerup. Defaults to 0.
+        """
+        description = ( ( 'OpenBubble', f'({ value }) Bubble' ),
+                        'Reveals random bubble, if there is one' )
         super().__init__( coords, color, description, value )
 
-    def apply_powerup( self, window : pg.Surface, minefield: Minefield ) -> bool:
+    def apply_powerup( self, window : pg.Surface, minefield: Minefield ) -> str:
+        """
+        __Overrided parent method, that applies powerup__\n
+        Opens random bubble ( tile with zero mines around ) if there is one
+        Args:
+            window (pg.Surface): pygame window/surface on which powerup button should be print
+            minefield (Minefield): minefield on which are powerups applied
+        Returns:
+            str: 'applied', 'used', 'not_used'
+        """
         c_click = minefield.mouse_pos_to_coords( pg.mouse.get_pos() )
         if c_click != OUT_OF_BOUNDS:
             check = 0
@@ -89,12 +163,35 @@ class OpenBubble( Powerup ):
         return 'not_used'
 
 class CrossOpen( Powerup ):
-    def __init__( self, coords : tuple, color : tuple, value, cross_range ):
-        description = ( ( 'CrossOpen', f'({ value }) Cross' ), f'Reveals + from selected tile' )
+    """
+    __Class inherited from class Powerup__\n
+    Safely opens tiles in 4 directions from clicked tile forming a cross
+    """
+    def __init__( self, coords : tuple, color : tuple, value, cross_range : np.uint32 ):
+        """
+        __Constructor for class instance__\n
+        Creates and initializes powerup class instance\n
+        Args:
+            coords (tuple): coordinates for powerup button
+            color (tuple): text color
+            value (int, optional): cost of powerup. Defaults to 0.
+            cross_range (int): range of cross
+        """
+        description = ( ( 'CrossOpen', f'({ value }) Cross' ),
+                        'Reveals cross from selected tile' )
         super().__init__( coords, color, description, value )
         self.m_range = cross_range
 
-    def apply_powerup( self, window : pg.Surface, minefield: Minefield ) -> bool:
+    def apply_powerup( self, window : pg.Surface, minefield: Minefield ) -> str:
+        """
+        __Overrided parent method, that applies powerup__\n
+        Safely opens tiles in 4 directions from clicked tile forming a cross
+        Args:
+            window (pg.Surface): pygame window/surface on which powerup button should be print
+            minefield (Minefield): minefield on which are powerups applied
+        Returns:
+            str: 'applied', 'used', 'not_used'
+        """
         c_click = minefield.mouse_pos_to_coords( pg.mouse.get_pos() )
         if c_click != OUT_OF_BOUNDS:
             if minefield.m_field[c_click].is_open():
@@ -112,15 +209,12 @@ class CrossOpen( Powerup ):
             col = minefield.m_field[ min_y : max_y, c_click[1] ]
             row = minefield.m_field[ c_click[0], min_x : max_x ]
 
-            #col = minefield.m_field[ 0 : field_dim[0], c_click[1] ]
-            #row = minefield.m_field[ c_click[0], 0 : field_dim[1] ]
             cross = np.append( col, row )
             for tile in cross:
                 if not tile.is_flag():
                     if tile.is_mine():
                         tile.flag()
                     else:
-                        #tile.open()
                         minefield.open( tile.arr_coords() )
 
             minefield.display_field( window )
@@ -129,12 +223,35 @@ class CrossOpen( Powerup ):
         return 'not_used'
 
 class FlagRandom( Powerup ):
+    """
+    __Class inherited from class Powerup__\n
+    Randomly flags m mines or does nothing if number of mines left to flag is less then m
+    """
     def __init__( self, coords : tuple, color : tuple, value, mines : np.uint32 ):
-        description = ( ( 'FlagRandom', f'({ value }) Flag_{mines}' ), f'Randomly flags {mines} mines' )
+        """
+        __Constructor for class instance__\n
+        Creates and initializes powerup class instance\n
+        Args:
+            coords (tuple): coordinates for powerup button
+            color (tuple): text color
+            value (int, optional): cost of powerup. Defaults to 0.
+            mines
+        """
+        description = ( ( 'FlagRandom', f'({ value }) Flag_{mines}' ),
+                        f'Randomly flags {mines} mines' )
         super().__init__( coords, color, description, value )
         self.m_mines = mines
 
-    def apply_powerup( self, window : pg.Surface, minefield: Minefield ) -> bool:
+    def apply_powerup( self, window : pg.Surface, minefield: Minefield ) -> str:
+        """
+        __Overrided parent method, that applies powerup__\n
+        Randomly flags m mines or does nothing if number of mines left to flag is less then m
+        Args:
+            window (pg.Surface): pygame window/surface on which powerup button should be print
+            minefield (Minefield): minefield on which are powerups applied
+        Returns:
+            str: 'applied', 'used', 'not_used'
+        """
         c_click = minefield.mouse_pos_to_coords( pg.mouse.get_pos() )
         if c_click != OUT_OF_BOUNDS:
             non_flaged_mines = []
@@ -146,11 +263,6 @@ class FlagRandom( Powerup ):
                 indexes = rd.sample( range( 0, len( non_flaged_mines ) ), self.m_mines )
                 for idx in indexes:
                     non_flaged_mines[idx].flag()
-
-            #boom_chance = random_bool( 0.1 )
-            #if boom_chance:
-            #    minefield.open_mines()
-            #    minefield.m_status = 'l'
 
             minefield.display_field( window )
             return 'applied'
